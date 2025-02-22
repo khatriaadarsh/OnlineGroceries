669ee82fb8a95ecb50b74e00198b9b9fd73db04c
khatriaadarsh <aadarshkumark2003@gmail.com>
2025-02-23 01:28:56 +0500

    Learn ScrollView

--- App.jsx
+++ App.jsx
@@ -1,43 +1,17 @@
-import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-// import Practice from './Practice';
+import {SafeAreaView, Text, StyleSheet, StatusBar} from 'react-native';
+
 const App = () => {
   return (
-    <View style={styles.container}>
-      {/* <Practice /> */}
-      {/* <Text style={styles.text}>App</Text> */}
-      <View style={styles.box01} />
-      <View style={styles.box02} />
-      <View style={styles.box03} />
-    </View>
+    <SafeAreaView style={style.container}>
+      <Text>Hello Bhai</Text>
+    </SafeAreaView>
   );
 };
-
 export default App;
 
-const styles = StyleSheet.create({
+const style = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: 'black',
-    paddingTop: 40,
-    flexDirection: 'col',
-    justifyContent: 'center',
-    alignItems: 'center',
-    gap: 10,
-  },
-  box01: {
-    width: 80,
-    height: 80,
-    backgroundColor: 'red',
-  },
-  box02: {
-    width: 80,
-    height: 80,
-    backgroundColor: 'yellow',
-  },
-  box03: {
-    width: 80,
-    height: 80,
-    backgroundColor: 'gray',
+    paddingTop: StatusBar.currentHeight,
   },
 });
