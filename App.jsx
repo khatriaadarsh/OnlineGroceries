9c25aa9feff141ce05b5eb6b569eabc435bcec45
khatriaadarsh <aadarshkumark2003@gmail.com>
2025-02-25 02:16:14 +0500

    Learn useState using this get text from input print on click of btn

--- App.jsx
+++ App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import {useState} from 'react';
 import {
   SafeAreaView,
   View,
@@ -6,20 +6,49 @@ import {
   StyleSheet,
   StatusBar,
   Image,
+  FlatList,
+  TextInput,
+  Button,
 } from 'react-native';
-
 const App = () => {
+  const data = [
+    {id: '1', name: 'Apple'},
+    {id: '2', name: 'Banana'},
+    {id: '3', name: 'Cherry'},
+    {id: '4', name: 'Date'},
+    {id: '5', name: 'Elderberry'},
+    {id: '6', name: 'Fig'},
+    {id: '7', name: 'Grape'},
+    {id: '8', name: 'Honeydew'},
+  ];
+  const [result, setResult] = useState('');
+  const [submitText, setSubmitText] = useState('');
+  const handleSubmit = () => {
+    setSubmitText(result);
+    setResult('');
+  };
   return (
     <SafeAreaView style={style.container}>
-      <View style={style.card}>
-        <Image
-          style={{width: 40, height: 40, borderRadius: 40}}
-          source={{
-            uri: 'https://plus.unsplash.com/premium_photo-1669703777437-27602d656c27?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-          }}></Image>
-        <Text>Name</Text>
-        <Text>Email</Text>
-      </View>
+      {/* <FlatList
+        data={data}
+        renderItem={({item}) => (
+          <View style={style.item}>
+            <Text style={style.text}>{item.name}</Text>
+          </View>
+        )}
+        keyExtractor={item => item.id}
+      /> */}
+
+      <Text style={style.welText}>Welcome to Input text Application</Text>
+      <TextInput
+        placeholder="Enter text here..."
+        style={style.inputText}
+        value={result}
+        onChangeText={setResult}
+        multiline
+      />
+      <Button title="Submit" onPress={handleSubmit}></Button>
+      {submitText ? <Text>Result:{submitText}</Text> : null}
     </SafeAreaView>
   );
 };
@@ -28,19 +57,49 @@ export default App;
 const style = StyleSheet.create({
   container: {
     paddingTop: StatusBar.currentHeight,
-    width: '100%',
-    height: '100%',
-    backgroundColor: 'gray',
-    paddingVertical: 10,
-    paddingHorizontal: 5,
-  },
-  card: {
-    width: 100,
-    height: 100,
-    backgroundColor: 'white',
-    borderRadius: 10,
-    padding: 5,
+    flex: 1,
+    padding: 16,
     alignItems: 'center',
+    backgroundColor: 'gray',
     justifyContent: 'center',
+    gap: 10,
   },
+  welText: {
+    fontWeight: 'bold',
+    fontSize: 19,
+  },
+  inputText: {
+    width: '100%',
+    borderWidth: 1,
+    padding: 5,
+    borderRadius: 10,
+  },
+  // item: {
+  //   padding: 16,
+  //   marginVertical: 8,
+  //   backgroundColor: 'white',
+  //   borderRadius: 8,
+  // },
+  // text: {
+  //   fontSize: 18,
+  // },
 });
+// card: {
+//   width: 100,
+//   height: 100,
+//   backgroundColor: 'white',
+//   borderRadius: 10,
+//   padding: 5,
+//   alignItems: 'center',
+//   justifyContent: 'center',
+// },
+
+/* <View style={style.card}>
+<Image
+  style={{width: 40, height: 40, borderRadius: 40}}
+  source={{
+    uri: 'https://plus.unsplash.com/premium_photo-1669703777437-27602d656c27?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  }}></Image>
+<Text>Name</Text>
+<Text>Email</Text>
+</View> */
